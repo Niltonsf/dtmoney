fcaf4a9c8042710c32814a63939591232db920a8
nilton.schumacher <nilton.schumacher@alta-rt.com>
2021-09-22 13:25:13 -0300

    Finished the Modal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-import { Dashboard } from './components/Dashboard'
+import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+	
+	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+	function handleOpenNewTransactionModal() {
+		setIsNewTransactionModalOpen(true);
+	}
+	
+	function handleCloseNewTransactionModal() {
+		setIsNewTransactionModalOpen(false);
+	}
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
 			<Dashboard />
+
+			<NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+			
 			<GlobalStyle />
     </>
   );
